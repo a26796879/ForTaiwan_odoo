aec8a2b8b27605560845ffee020f89179706feab
a26796879 <a26796879@gmail.com>
2021-10-23 14:49:38 +0000

    fix config error

--- line-notify/models/news_crawler.py
+++ line-notify/models/news_crawler.py
@@ -4,9 +4,7 @@ from newspaper import Article
 from datetime import datetime,timedelta
 import requests, json
 from bs4 import BeautifulSoup
-import os
-from dotenv import load_dotenv
-load_dotenv()
+import config
 
 
 class news_crawler(models.Model):
@@ -55,7 +53,7 @@ class news_crawler(models.Model):
                             })
                             if create_record:
                                 #發送 Line Notify 訊息
-                                token = os.getenv('line_token')  # MySelf
+                                token = config.line_token()  # MySelf
                                 self.lineNotify(token, news[i]['title'] + " " + news[i]['url'])
     def get_udn_news(self,keyword):
         udn_url = 'https://udn.com/api/more?page=0&id=search:%E5%9F%BA%E9%80%B2&channelId=2&type=searchword'
@@ -97,7 +95,7 @@ class news_crawler(models.Model):
                         })
                         if create_record:
                             #發送 Line Notify 訊息
-                            token = os.getenv('line_token')  # MySelf
+                            token = config.line_token()  # MySelf
                             self.lineNotify(token, title + " " + url)
     def get_apple_news(self,keyword):
         apple_url = 'https://tw.appledaily.com/pf/api/v3/content/fetch/search-query?query=%7B%22searchTerm%22%3A%22%25E5%259F%25BA%25E9%2580%25B2%22%2C%22start%22%3A-1%7D&d=262'
@@ -134,7 +132,7 @@ class news_crawler(models.Model):
                     })
                     if create_record:
                         #發送 Line Notify 訊息
-                        token = os.getenv('line_token')  # MySelf
+                        token = config.line_token()  # MySelf
                         self.lineNotify(token, title + " " + url)
     def get_ltn_news(self,keyword):
         url = 'https://search.ltn.com.tw/list?keyword=%E5%9F%BA%E9%80%B2'
@@ -177,7 +175,7 @@ class news_crawler(models.Model):
                             })
                         if create_record:
                             #發送 Line Notify 訊息
-                            token = os.getenv('line_token')  # MySelf
+                            token = config.line_token()  # MySelf
                             self.lineNotify(token, title + " " + url)
     def get_setn_news(self,keyword):
         url = 'https://www.setn.com/search.aspx?q=%E5%9F%BA%E9%80%B2&r=0'
@@ -218,7 +216,7 @@ class news_crawler(models.Model):
                                 })
                         if create_record:
                             #發送 Line Notify 訊息
-                            token = os.getenv('line_token')  # MySelf
+                            token = config.line_token()  # MySelf
                             self.lineNotify(token, title + " " + url)
     def get_ettoday_news(self,keyword):
         url = 'https://www.ettoday.net/news_search/doSearch.php?search_term_string=%E5%9F%BA%E9%80%B2'
@@ -260,7 +258,7 @@ class news_crawler(models.Model):
                                 })
                         if create_record:
                             #發送 Line Notify 訊息
-                            token = os.getenv('line_token')  # MySelf
+                            token = config.line_token()  # MySelf
                             self.lineNotify(token, title + " " + url)
     def get_TVBS_news(self,keyword):
         url = 'https://news.tvbs.com.tw/news/searchresult/%E5%9F%BA%E9%80%B2/news'
@@ -302,7 +300,7 @@ class news_crawler(models.Model):
                                 })
                         if create_record:
                             #發送 Line Notify 訊息
-                            token = os.getenv('line_token')  # MySelf
+                            token = config.line_token()  # MySelf
                             self.lineNotify(token, title + " " + url)
     def get_china_news(self,keyword):
         links = ['https://www.chinatimes.com/search/%E9%99%B3%E6%9F%8F%E6%83%9F?chdtv','https://www.chinatimes.com/search/%E5%9F%BA%E9%80%B2?chdtv']
@@ -341,5 +339,5 @@ class news_crawler(models.Model):
                                     })
                             if create_record:
                                 #發送 Line Notify 訊息
-                                token = os.getenv('line_token')  # MySelf
+                                token = config.line_token()  # MySelf
                                 self.lineNotify(token, title + " " + url)
