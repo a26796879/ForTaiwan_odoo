9b8b07e31ff900ae2464952bfcb08e4f62559095
a26796879 <a26796879@gmail.com>
2022-02-12 00:58:12 +0800

    set Float field digits8

--- extra-addons/vote_map_data/models/icon_settings.py
+++ extra-addons/vote_map_data/models/icon_settings.py
@@ -6,8 +6,8 @@ class icon_settings(models.Model):
     _description = '選舉地圖 座標設定'
 
     name = fields.Char('名稱')
-    local_x = fields.Float('座標x')
-    local_y = fields.Float('座標y')
+    local_x = fields.Float('座標x',digits=(12,8))
+    local_y = fields.Float('座標y',digits=(12,8))
     icon_url = fields.Char('icon_url設定')
 
 '''
