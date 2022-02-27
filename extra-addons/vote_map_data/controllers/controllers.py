39de1a560f505fd960c213bdf108578531c2a4de
a26796879 <a26796879@gmail.com>
2022-02-27 16:02:44 +0800

    add api for get map & vote data

--- extra-addons/vote_map_data/controllers/controllers.py
+++ extra-addons/vote_map_data/controllers/controllers.py
@@ -5,14 +5,27 @@ _logger = logging.getLogger(__name__)
 
 class Icon_Settings(http.Controller):
     @http.route('/objects', type="http", auth="public", methods=["GET"], csrf=False, cors='*')
-    def index(self):
+    def icon_settings(self):
         dataset = http.request.env['icon_settings'].sudo().search([])
-
         result = []
         for data in dataset:
             _logger.debug(data.name)
             result.append({'name':data.name,'local':[data.local_x,data.local_y],'icon_url':data.icon_url})
         results = json.dumps({'data':result})
+        return results
+
+    @http.route('/vote_data', type="http", auth="public", methods=["GET"], csrf=False, cors='*')
+    def vote_data(self):
+        dataset = http.request.env['vote_data'].sudo().search([('city','=','高雄市'),('district','=','三民區')])
+        str_results = json.dumps(dataset.data)
+        results = json.loads(str_results)
+        return results
+
+    @http.route('/map_data', type="http", auth="public", methods=["GET"], csrf=False, cors='*')
+    def map_data(self):
+        dataset = http.request.env['map_data'].sudo().search([('city','=','高雄市'),('district','=','三民區')])
+        str_results = json.dumps(dataset.data)
+        results = json.loads(str_results)
         return results
 
 
