658fc94c2e9d1f02ff883f42029a1813e7123485
a26796879 <a26796879@gmail.com>
2022-03-19 22:29:27 +0800

    add place as 選區 for API

--- extra-addons/vote_map_data/controllers/controllers.py
+++ extra-addons/vote_map_data/controllers/controllers.py
@@ -9,7 +9,6 @@ class Icon_Settings(http.Controller):
         dataset = http.request.env['icon_settings'].sudo().search([])
         result = []
         for data in dataset:
-            _logger.debug(data.name)
             result.append({'name':data.name,'local':[data.local_x,data.local_y],'icon_url':data.icon_url})
         results = json.dumps({'data':result})
         return results
@@ -20,28 +19,26 @@ class Icon_Settings(http.Controller):
         citys = []
         for i in dataset:
             citys.append(i.city)
-            _logger.debug(i.city)
         results = json.dumps({'data':citys})
-        _logger.debug(results)
         return results
 
     @http.route('/vote_data/<string:vote_type>/<string:city>', type="http", auth="public", methods=["GET"], csrf=False, cors='*')
     def vote_data_city(self,vote_type,city):
         dataset = http.request.env['vote_data'].sudo().search([('vote_type','=',vote_type),('city','=',city)])
-        vote_places = []
+        places = []
         for i in dataset:
-            vote_places.append(i.vote_place)
-            _logger.debug(i.vote_place)
-        results = json.dumps({'data':vote_places})
-        _logger.debug(results)
+            places.append(i.place)
+        results = json.dumps({'data':places})
         return results
 
     @http.route('/vote_data/<string:vote_type>/<string:city>/<string:place>', type="http", auth="public", methods=["GET"], csrf=False, cors='*')
     def vote_data_place(self,vote_type,city,place):
         dataset = http.request.env['vote_data'].sudo().search([('vote_type','=',vote_type),('city','=',city),('place','=',place)])
-        str_results = json.dumps(dataset.data)
-        results = json.loads(str_results)
-        return results
+        districts = []
+        for i in dataset:
+            districts.append(i.district)
+        results = json.dumps({'data':districts})
+        return 'results'
     #
     @http.route('/vote_data/<string:vote_type>/<string:city>/<string:district>', type="http", auth="public", methods=["GET"], csrf=False, cors='*')
     def vote_data_district(self,vote_type,city,district):
@@ -56,9 +53,7 @@ class Icon_Settings(http.Controller):
         dataset = http.request.env['map_data'].sudo().search([('city','=',city)])
         for i in dataset:
             districts.append(i.district)
-            _logger.debug(i.district)
         results = json.dumps({'data':districts})
-        _logger.debug(results)
         return results
 
     @http.route('/map_data/<string:city>/<string:district>', type="http", auth="public", methods=["GET"], csrf=False, cors='*')
