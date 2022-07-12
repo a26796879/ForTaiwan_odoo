8bd66c9ebc86c80795716906d03fc5503d490100
a26796879 <a26796879@gmail.com>
2022-07-12 18:27:27 +0800

    follow PEP8

--- extra-addons/vote_map_data/controllers/controllers.py
+++ extra-addons/vote_map_data/controllers/controllers.py
@@ -1,21 +1,26 @@
 # -*- coding: utf-8 -*-
+import json
+import logging
 from odoo import http
-import json, logging
 _logger = logging.getLogger(__name__)
 
 class IconSettings(http.Controller):
     '''for setting icon on map'''
     @http.route('/objects', type="http", auth="public", methods=["GET"], csrf=False, cors='*')
     def icon_settings(self):
+        ''' get icon settings'''
         dataset = http.request.env['icon_settings'].sudo().search([])
         result = []
         for data in dataset:
-            result.append({'name':data.name,'local':[data.local_x,data.local_y],'icon_url':data.icon_url})
+            result.append({
+                    'name':data.name,'local':[data.local_x,data.local_y],'icon_url':data.icon_url})
         results = json.dumps({'data':result})
         return results
 
-    @http.route('/vote_data/<string:vote_type>', type="http", auth="public", methods=["GET"], csrf=False, cors='*')
+    @http.route('/vote_data/<string:vote_type>'
+                , type="http", auth="public", methods=["GET"], csrf=False, cors='*')
     def vote_data_vote_type(self,vote_type):
+        ''' get vote data by vote type'''
         dataset = http.request.env['vote_data'].sudo().search([('vote_type','=',vote_type)])
         citys = []
         for i in dataset:
@@ -23,33 +28,50 @@ class IconSettings(http.Controller):
         results = json.dumps({'data':citys})
         return results
 
-    @http.route('/vote_data/<string:vote_type>/<string:city>', type="http", auth="public", methods=["GET"], csrf=False, cors='*')
+    @http.route('/vote_data/<string:vote_type>/<string:city>'
+                , type="http", auth="public", methods=["GET"], csrf=False, cors='*')
     def vote_data_city(self,vote_type,city):
-        dataset = http.request.env['vote_data'].sudo().search([('vote_type','=',vote_type),('city','=',city)])
+        ''' get vote data by vote city'''
+        dataset = http.request.env['vote_data'].sudo().search(
+            [('vote_type','=',vote_type)
+            ,('city','=',city)])
         places = []
         for i in dataset:
             places.append(i.place)
         results = json.dumps({'data':places})
         return results
 
-    @http.route('/vote_data/<string:vote_type>/<string:city>/<string:place>', type="http", auth="public", methods=["GET"], csrf=False, cors='*')
+    @http.route('/vote_data/<string:vote_type>/<string:city>/<string:place>',
+                 type="http", auth="public", methods=["GET"], csrf=False, cors='*')
     def vote_data_get_place(self,vote_type,city,place):
-        dataset = http.request.env['vote_data'].sudo().search([('vote_type','=',vote_type),('city','=',city),('place','=',place)])
+        ''' get vote data by vote place'''
+        dataset = http.request.env['vote_data'].sudo().search(
+            [('vote_type','=',vote_type)
+            ,('city','=',city)
+            ,('place','=',place)])
         districts = []
         for i in dataset:
             districts.append(i.district)
         results = json.dumps({'data':districts})
         return results
 
-    @http.route('/vote_data/<string:vote_type>/<string:city>/<string:place>/<string:district>', type="http", auth="public", methods=["GET"], csrf=False, cors='*')
+    @http.route('/vote_data/<string:vote_type>/<string:city>/<string:place>/<string:district>',
+                 type="http", auth="public", methods=["GET"], csrf=False, cors='*')
     def vote_data_get_district(self,vote_type,city,place,district):
-        dataset = http.request.env['vote_data'].sudo().search([('vote_type','=',vote_type),('city','=',city),('place','=',place),('district','like',district)])
+        ''' get vote data by vote district'''
+        dataset = http.request.env['vote_data'].sudo().search(
+            [('vote_type','=',vote_type)
+            ,('city','=',city)
+            ,('place','=',place)
+            ,('district','like',district)])
         str_results = json.dumps(dataset.data)
         results = json.loads(str_results)
         return results
     #
-    @http.route('/map_data/<string:city>', type="http", auth="public", methods=["GET"], csrf=False, cors='*')
+    @http.route('/map_data/<string:city>', type="http", auth="public",
+                 methods=["GET"], csrf=False, cors='*')
     def map_data_city(self,city):
+        ''' get map data by city'''
         districts = []
         dataset = http.request.env['map_data'].sudo().search([('city','=',city)])
         for i in dataset:
@@ -57,23 +79,12 @@ class IconSettings(http.Controller):
         results = json.dumps({'data':districts})
         return results
 
-    @http.route('/map_data/<string:city>/<string:district>', type="http", auth="public", methods=["GET"], csrf=False, cors='*')
+    @http.route('/map_data/<string:city>/<string:district>', type="http",
+                 auth="public", methods=["GET"], csrf=False, cors='*')
     def map_data_district(self,city,district):
-        dataset = http.request.env['map_data'].sudo().search([('city','=',city),('district','=',district)])
+        ''' get map data by district'''
+        dataset = http.request.env['map_data'].sudo().search(
+                [('city','=',city),('district','=',district)])
         str_results = json.dumps(dataset.data)
         results = json.loads(str_results)
         return results
-
-
-#     @http.route('/line-notify/line-notify/objects/', auth='public')
-#     def list(self, **kw):
-#         return http.request.render('line-notify.listing', {
-#             'root': '/line-notify/line-notify',
-#             'objects': http.request.env['line-notify.line-notify'].search([]),
-#         })
-
-#     @http.route('/line-notify/line-notify/objects/<model("line-notify.line-notify"):obj>/', auth='public')
-#     def object(self, obj, **kw):
-#         return http.request.render('line-notify.object', {
-#             'object': obj
-#         })
